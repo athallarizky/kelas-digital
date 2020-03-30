eece22ba4cf854a331e76530ce1c6cfd1581127a
athallarizky <athallarizk@gmail.com>
2020-03-30 18:57:47 +0700

    Login Page Styling

--- src/components/Auth/Register/style.js
+++ src/components/Auth/Register/style.js
@@ -1,19 +1,59 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-        width: '100%',
-    },
     wrapper:{
+        display:'flex',
         height: '100vh',
-        paddingTop: 80,
-        backgroundColor:'#F5F6F8'
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#E5E5E5',
     },
-    paper: {
-        padding: theme.spacing(2),
-        backgroundColor:'transparent',
-        boxShadow:'none',
-        textAlign:'left'
+    boxContainer: {
+      backgroundColor: '#FFF',
+      borderRadius:10,
+      minWidth:'450px',
+      height:'80%',
+      padding: '50px 100px',
     },
+
+    appLogo:{
+        marginTop: 10,
+        marginBottom:30,
+        color:'#3F3D56',
+        fontFamily:'Inter-Bold',
+        fontSize:15,
+    },
+    mainText:{
+        fontSize:45,
+        marginBottom:20,
+    },
+    secondaryText:{
+        color:'#3F3D56',
+        maxWidth:'70%',
+        marginBottom:20,
+    },
+
+    labelInput:{
+        fontWeight:'bold',
+    },
+    registerInput:{
+        display:'block',
+        marginTop:5,
+        marginBottom:20,
+        minWidth:'100%',
+        border:'2px solid #FF2858',
+        borderRadius:5,
+    },
+    buttonInput:{
+        height:50,
+        fontFamily:'Inter-Bold',
+    },
+    loginText:{
+        textAlign:'center',
+        padding:'20px 0',
+    },
+    loginLink:{
+        textDecoration:'none',
+        fontFamily:'Inter-SemiBold',
+    }
 })); 
